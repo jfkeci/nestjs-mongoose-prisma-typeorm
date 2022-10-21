76a97ad39e652c2b771bd3e483968161c576735a
jfkeci <ja.lip132@gmail.com>
2022-10-21 08:28:14 +0200

    feat: serve client with nestjs - project setup - remove client build from gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/dist')
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
